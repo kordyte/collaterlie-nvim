9e1cc8a366e9457a91c033c9a417695fd15f015a
Martin Jones <martin@ignition-training.com>
2024-08-26 03:02:51 +0000

    .

--- lua/collaterlie.lua
+++ lua/collaterlie.lua
@@ -7,22 +7,6 @@ local function hi(fg, bg, special, specialcol)
 end
 
 local fg = {
-  normal = '#432b04',
-  highlight = '#484848',
-  dialogborder = '#7f5107',
-  selected = '#484848',
-  popup = '#432b04',
-  addition = '#0f801e',
-  deletion = '#801010',
-  change = '#7f7f03',
-  diff = '#780f81',
-  directory = '#192ce2',
-  diagnosticerror = '#e31c1c',
-  diagnosticwarn = '#7f7f03',
-  diagnosticinfo = '#117980',
-  diagnostichint = '#780f81',
-  diagnosticok = '#0f801e',
-
   syn_literal1 = '#0f801e',
   syn_literal2 = '#08440f',
   syn_function1 = '#432b04',
@@ -45,23 +29,6 @@ local fg = {
 }
 
 local bg = {
-  normal = '#fdfdfc',
-  highlight = '#cbff97',
-  cursorline = '#dee1fa',
-  gutter = '#ededed',
-  dialog = '#fffaf6',
-  popup = '#fff8ff',
-  selected = '#edffdb',
-  addition = '#defbe1',
-  deletion = '#fddddd',
-  change = '#fefedc',
-  diff = '#f9defc',
-  diagnosticerror = '#fddddd',
-  diagnosticwarn = '#fefedc',
-  diagnosticinfo = '#ddfafc',
-  diagnostichint = '#f9defc',
-  diagnosticok = '#defbe1',
-
   syn_variable = '#fffaff', -- The background might only be applied for some highlights 
   syn_preproc = '#fffafa',
   syn_metadata = '#fffafa',
@@ -69,6 +36,8 @@ local bg = {
   syn_keyword1 = '#fafaff',
   syn_keyword2 = '#fffdfa',
 }
+
+nyi = hi('#000000', '#ff2233', 'bold,undercurl', '#00ff00') -- Not Yet Implemented
 
 normal = hi('#432b04', '#fefefd', nil, nil) -- Default text in edit area
 gutter = hi(normal.fg, '#f9f8f5', nil, nil) -- numbering, gitsigns, and others inside the edit area
@@ -118,7 +87,7 @@ local ui_group = {
     QuickFixLine = hi(nil, '#fddddd', nil, nil),
     
     WinSeparator = hi('#7f5107', '#f9f9f5', nil, nil),
-    VertSplit = 'WinSeparator',
+       VertSplit = 'WinSeparator',
           
           WinBar = hi(nil, gutter.bg, nil, nil),
         WinBarNC = hi(nil, nil, nil, nil),
@@ -133,6 +102,7 @@ local ui_group = {
      EndOfBuffer = external,    -- Bottom of screen when scrolled up
          NonText = hi('#f4a3a3', nil, nil, nil), 
       Whitespace = hi('#070e44', nil, nil, nil),
+      SpecialKey = hi('#0f801e', nil, nil, nil), 
 
           Visual = hi(nil, '#fef0dc', nil, nil),
        VisualNOS = hi(nil, '#ededed', nil, nil),  -- for when window does not have focus (X11 only)
@@ -152,21 +122,23 @@ local ui_group = {
       PmenuThumb = hi(nil, '#f0e0a0', nil, nil), -- Only observed this in completion dialogs
        PmenuSbar = hi(nil, '#f8f0d8', nil, nil),
 
-       NormalFloat = dialog,
-       FloatTitle = hi(normal.fg, nil, 'bold' ,nil),
-       FloatBorder = dialogborder,
-       FloatShadow = hi(nil, dialogborder.fg, nil, nil), -- background is blended to form the shadow
+     NormalFloat = dialog,
+      FloatTitle = hi(normal.fg, nil, 'bold' ,nil),
+     FloatBorder = dialogborder,
+     FloatShadow = hi(nil, dialogborder.fg, nil, nil), -- background is blended to form the shadow
+        WildMenu = dialog,
 
         ErrorMsg = hi('#fefedc', '#e31c1c', 'bold', nil),   -- error messages displayed on the command line
       WarningMsg = hi('#7f5107', '#f4a3a3', nil, nil),
          MoreMsg = hi('#000000', '#fddddd', nil, nil),
         Question = 'MoreMsg',
          ModeMsg = hi('#202020', '#f8d59e', nil, nil),
-         
-        SpellBad = hi(nil, bg.diagnosticerror, 'undercurl', fg.diagnosticerror),
-        SpellCap = hi(nil, nil, 'undercurl', fg.diagnosticwarn),
-       SpellRare = hi(nil, nil, 'undercurl', fg.diagnostichint),
-      SpellLocal = hi(nil, nil, 'undercurl', fg.diagnostichint),
+    MsgSeparator = external,
+
+        SpellBad = hi(nil, diagerror.fg, 'undercurl', diagerror.fg),
+        SpellCap = hi(nil, nil, 'undercurl', diagwarn.fg),
+       SpellRare = hi(nil, nil, 'undercurl', diaghint.fg),
+      SpellLocal = hi(nil, nil, 'undercurl', diaghint.fg),
 
        Directory = hi('#192ce2', nil, nil, nil),
 
@@ -174,8 +146,6 @@ local ui_group = {
       DiffDelete = diffdeletesign,
       DiffChange = diffchangesign,
         DiffText = diffchangetext,
-
-      SpecialKey = hi(p.green.dark1, nil, nil, nil),
 }
 
 local diagnostic_group = {
@@ -197,17 +167,17 @@ local diagnostic_group = {
    DiagnosticVirtualTextHint = diaghint,
      DiagnosticVirtualTextOk = diagok,
   
-  DiagnosticFloatingError = hi('#ffff00', '#000000', nil, nil),  -- Can be observed with :lua vim.diagnostic.open_float() (assuming you there are diagnostics)
-   DiagnosticFloatingWarn = diagwarn,
-   DiagnosticFloatingInfo = diaginfo,
-   DiagnosticFloatingHint = diaghint,
-     DiagnosticFloatingOk = diagok,
+     DiagnosticFloatingError = diagerror,  -- Can be observed with :lua vim.diagnostic.open_float() (assuming you there are diagnostics)
+      DiagnosticFloatingWarn = diagwarn,
+      DiagnosticFloatingInfo = diaginfo,
+      DiagnosticFloatingHint = diaghint,
+        DiagnosticFloatingOk = diagok,
   
-  DiagnosticSignError = diagerror, 
-   DiagnosticSignWarn = diagwarn,
-   DiagnosticSignInfo = diaginfo,
-   DiagnosticSignHint = diaghint,
-     DiagnosticSignOk = diagok,
+         DiagnosticSignError = diagerror, 
+          DiagnosticSignWarn = diagwarn,
+          DiagnosticSignInfo = diaginfo,
+          DiagnosticSignHint = diaghint,
+            DiagnosticSignOk = diagok,
 }
 
 local telescope_group = {
@@ -219,17 +189,234 @@ local telescope_group = {
         TelescopeMatching = highlighted,
 }
 
+
+
+-- Treesitter Capture Groups: https://github.com/nvim-treesitter/nvim-treesitter/blob/master/CONTRIBUTING.md
+-- LSP Groups Semantic Tokens: https://microsoft.github.io/language-server-protocol/specifications/lsp/3.17/specification/#textDocument_semanticTokens
+--
+-- Both TreeSitter and LSP can match. Priority is used in Neovim to decide which
+-- one is applied last.
+-- The order is:
+-- Treesitter (lowest priority)
+-- lsp.type
+-- lsp.mod
+-- lsp.typemod (highest priority)
+--
+-- LSP only provides type and mod, the typemod is a Neovim thing to allow
+-- you to do specific things for a combination
+
+syn_literal_string = hi('#08440f', nil, nil, nil)
+syn_literal_string_escape = hi('#0f801e', nil, 'bold', nil)
+syn_literal_string_special = literal_string_escape
+syn_literal_regex = hi('#0f801e', nil, nil, nil)
+syn_literal_character = hi('#0f801e', nil, nil, nil)
+syn_literal_character_special = hi('#0f801e', nil, 'bold', nil)
+syn_literal_number = hi("#0f801e", nil, nil, nil)
+syn_literal_boolean = hi("#0f801e", nil, nil, nil)
+syn_literal_float = hi("#0f801e", nil, nil, nil)
+
+syn_variable = hi('#410745', nil, nil, nil)
+syn_variable_builtin = hi('#780f81', nil, nil, nil) 
+syn_parameter = hi('#410745', '#fffaff', nil, nil)
+syn_field = hi('#410745', nil, nil, nil)
+syn_property = hi('#410745', nil, nil, nil)
+
+syn_namespace = hi('#08440f', nil, nil, nil)
+
+syn_punctuation = normal
+syn_punctuation_delimiter = normal
+syn_punctuation_bracket = hi('#780f81', nil, nil)
+syn_punctuation_special = hi('#0e187f', nil, nil)
+syn_operator = hi('#0e187f', nil, nil, nil)
+
+syn_kw1 = hi('#070e44', '#fafaff', nil, nil)
+syn_kw2 = hi('#0e187f', '#fffdfa', nil, nil)
+syn_kw1_2 = hi(syn_kw1.fg, syn_kw2.bg, nil, nil)
+syn_kw2_1 = hi(syn_kw2.fg, syn_kw1.bg, nil, nil)
+
+syn_keyword = syn_kw1
+syn_kw_conditional = syn_kw1 
+syn_kw_repeat = syn_kw1
+syn_kw_type = syn_kw1
+syn_kw_label = hi(syn_kw2.fg, syn_kw2.bg, 'bold', nil)
+syn_kw_debug = hi(syn_kw2.fg, syn_kw2.bg, 'bold', nil)
+syn_kw_return = syn_kw2_1
+syn_kw_function = syn_kw2_1
+syn_kw_coroutine = syn_kw2_1
+syn_kw_directive = syn_kw2
+syn_kw_exception = syn_kw1
+syn_kw_import = syn_kw1
+syn_kw_modifier = syn_kw1_2
+
+
+local lsp_group = {
+      ['@lsp.type.namespace'] = syn_namespace,
+           ['@lsp.type.type'] = nyi,
+          ['@lsp.type.class'] = nyi,
+           ['@lsp.type.enum'] = nyi,
+      ['@lsp.type.interface'] = nyi,
+         ['@lsp.type.struct'] = nyi,
+  ['@lsp.type.typeParameter'] = nyi,
+      ['@lsp.type.parameter'] = syn_parameter,
+       ['@lsp.type.variable'] = syn_variable,
+       ['@lsp.type.property'] = syn_property,
+     ['@lsp.type.enumMember'] = nyi,
+          ['@lsp.type.event'] = nyi,
+       ['@lsp.type.function'] = nyi,
+         ['@lsp.type.method'] = nyi,
+          ['@lsp.type.macro'] = nyi,
+        ['@lsp.type.keyword'] = syn_keyword,
+       ['@lsp.type.modifier'] = nyi,
+        ['@lsp.type.comment'] = nyi,
+         ['@lsp.type.string'] = syn_literal_string,
+         ['@lsp.type.number'] = syn_literal_number,
+         ['@lsp.type.regexp'] = syn_literal_regex,
+       ['@lsp.type.operator'] = syn_operator,
+      ['@lsp.type.decorator'] = nyi,
+}
+
+local treesitter_group = {
+                       ['@string'] = syn_literal_string,
+                ['@string.escape'] = syn_literal_string_escape,
+               ['@string.special'] = syn_literal_string_special,
+                ['@string.regexp'] = syn_literal_regex,
+                    ['@character'] = syn_literal_character,
+            ['@character.special'] = syn_literal_character_special,
+                      ['@boolean'] = syn_literal_boolean,
+                       ['@number'] = syn_literal_number,
+                 ['@number.float'] = syn_literal_float,
+                        ['@float'] = syn_literal_float,
+
+                     ['@variable'] = syn_variable,
+             ['@variable.builtin'] = syn_variable_builtin,
+           ['@variable.parameter'] = syn_parameter,
+                    ['@parameter'] = syn_parameter,
+          ['@parameter.reference'] = syn_parameter,
+   ['@variable.parameter.builtin'] = syn_parameter,
+            ['@parameter.builtin'] = syn_parameter,
+              ['@variable.member'] = syn_field,
+                       ['@member'] = syn_field,
+               ['@variable.field'] = syn_field,
+                        ['@field'] = syn_field,
+                    
+                    ['@namespace'] = syn_namespace,
+
+                  ['@punctuation'] = syn_punctuation,
+        ['@punctuation.delimiter'] = syn_punctuation_delimiter,
+          ['@punctuation.bracket'] = syn_punctuation_bracket,
+          ['@punctuation.special'] = syn_punctuation_special,
+                     ['@operator'] = syn_operator,
+
+                      ['@keyword'] = syn_keyword,
+            ['@keyword.coroutine'] = syn_kw_coroutine,
+             ['@keyword.function'] = syn_kw_function,
+             ['@keyword.operator'] = syn_operator,
+                     ['@operator'] = syn_operator,
+               ['@keyword.import'] = syn_kw_import,
+                 ['@keyword.type'] = syn_kw_type,
+             ['@keyword.modifier'] = syn_kw_modifier,
+               ['@keyword.repeat'] = syn_kw_repeat,
+                       ['@repeat'] = syn_kw_repeat,
+               ['@keyword.return'] = syn_kw_return,
+                ['@keyword.debug'] = syn_kw_debug,
+            ['@keyword.exception'] = syn_kw_exception,
+                    ['@exception'] = syn_kw_exception,
+          ['@keyword.conditional'] = syn_kw_conditional,
+                  ['@conditional'] = syn_kw_conditional,
+  ['@keyword.conditional.ternary'] = syn_kw_conditional,
+            ['@keyword.directive'] = syn_kw_directive,
+     ['@keyword.directive.define'] = syn_kw_directive,
+
+                     ['@constant'] = nyi,
+             ['@constant.builtin'] = nyi,
+               ['@constant.macro'] = nyi,
+                       ['@module'] = nyi,
+               ['@module.builtin'] = nyi,
+                        ['@label'] = nyi,
+
+         ['@string.documentation'] = nyi,
+        ['@string.special.symbol'] = nyi,
+           ['@string.special.url'] = nyi,
+          ['@string.special.path'] = nyi,
+
+
+
+                         ['@type'] = nyi,
+                 ['@type.builtin'] = nyi,
+              ['@type.definition'] = nyi,
+               ['@type.qualifier'] = nyi,
+                 ['@storageclass'] = nyi,
+
+                    ['@attribute'] = nyi,
+            ['@attribute.builtin'] = nyi,
+                     ['@property'] = nyi,
+
+                     ['@function'] = nyi,
+             ['@function.builtin'] = nyi,
+                ['@function.call'] = nyi,
+               ['@function.macro'] = nyi,
+
+['@include'] = nyi,
+['@define'] = nyi,
+['@preproc'] = nyi,
+
+
+              ['@function.method'] = nyi,
+         ['@function.method.call'] = nyi,
+
+                  ['@constructor'] = nyi,
+
+
+
+
+
+                      ['@comment'] = nyi,
+        ['@comment.documentation'] = nyi,
+
+                ['@comment.error'] = nyi,
+              ['@comment.warning'] = nyi,
+                 ['@comment.todo'] = nyi,
+                 ['@comment.note'] = nyi,
+                 
+                ['@markup.strong'] = nyi,
+                ['@markup.italic'] = nyi,
+         ['@markup.strikethrough'] = nyi,
+             ['@markup.underline'] = nyi,
+                 
+               ['@markup.heading'] = nyi,
+             ['@markup.heading.1'] = nyi,
+             ['@markup.heading.2'] = nyi,
+             ['@markup.heading.3'] = nyi,
+             ['@markup.heading.4'] = nyi,
+             ['@markup.heading.5'] = nyi,
+             ['@markup.heading.6'] = nyi,
+                 
+                 ['@markup.quote'] = nyi,
+                  ['@markup.math'] = nyi,
+                 
+                  ['@markup.link'] = nyi,
+            ['@markup.link.label'] = nyi,
+              ['@markup.link.url'] = nyi,
+                 
+                   ['@markup.raw'] = nyi,
+             ['@markup.raw.block'] = nyi,
+                 
+                  ['@markup.list'] = nyi,
+          ['@markup.list.checked'] = nyi,
+        ['@markup.list.unchecked'] = nyi,
+                 
+                    ['@diff.plus'] = nyi,
+                   ['@diff.minus'] = nyi,
+                   ['@diff.delta'] = nyi,
+                 
+                          ['@tag'] = nyi,
+                  ['@tag.builtin'] = nyi,
+                ['@tag.attribute'] = nyi,
+                ['@tag.delimiter'] = nyi,
+}
+
 local scheme_group = {
 
-    
-  
-    MsgSeparator = hi(p.grey.white, p.grey.dark1, nil, nil),
-         --  Title = hi(p.orange.vibrant, nil, nil, nil),
-        WildMenu = hi(p.grey.black, p.yellow.pale, nil, nil),
-
-            Todo = hi(p.orange.dark1, p.yellow.light, 'bold', nil),
-
--- ================== Diagnostic =========================
 -- ================== DAP =========================
 
                       DapSign = hi(p.orange.vibrant, p.grey.pale, nil, nil),
@@ -241,7 +428,7 @@ local scheme_group = {
                     DapUIType = '@type',
                    DapUIValue = hi(p.green.dark2, nil, nil, nil), 
            DapUIModifiedValue = hi(p.green.dark2, p.chartreuse.light, nil, nil), 
-              DapUIDecoration = hi(p.grey.vibrant, nil, nil, nil),
+              DapUIDecoration = hi(p.grey.vibrant, nil, nil, nil), 
                   DapUIThread = hi(p.red.dark1, nil, nil, nil),
            DapUIStoppedThread = hi(p.orange.dark1, p.orange.pale, nil, nil),
                DapUIFrameName = hi(p.orange.dark1, nil, nil, nil),
@@ -272,11 +459,11 @@ local scheme_group = {
          Comment = hi(p.orange.light, nil, 'italic', nil),
 
         Constant = hi(p.green.dark2, nil, nil, nil),
-          String = hi(p.green.dark2, nil, nil, nil),
-       Character = hi(p.green.dark1, nil, nil, nil),
-          Number = hi(p.green.dark1, nil, nil, nil),
-         Boolean = hi(p.green.dark1, nil, nil, nil),
-           Float = hi(p.green.dark1, nil, nil, nil),
+          String = literal_string,
+       Character = literal_character,
+          Number = literal_number,
+         Boolean = literal_boolean,
+           Float = literal_float,
 
       Identifier = hi(p.purple.dark2, nil, nil, nil),
         Function = hi(p.orange.dark1, nil, nil, nil),
@@ -312,15 +499,12 @@ local scheme_group = {
           Ignore = hi(nil, nil, nil, nil),
 
            Error = hi(p.red.vibrant, p.red.pale, 'bold,underline', nil),  -- any erroneous construct
+            Todo = hi(p.orange.dark1, p.yellow.light, 'bold', nil),
 
     -- Treesitter  ===============================================================================
 
-                ['@comment'] = hi(fg.syn_comment, nil, 'italic', nil), 
-            ['@punctuation'] = hi(fg.normal, nil, nil, nil),
-  ['@punctuation.delimiter'] = hi(fg.normal, nil, nil, nil),
-    ['@punctuation.bracket'] = hi(p.purple.dark2, nil, nil, nil),
-    ['@punctuation.special'] = hi(p.blue.dark1, nil, nil, nil),
-    
+ --[[               ['@comment'] = hi(fg.syn_comment, nil, 'italic', nil), 
+   XX 
              ['@text.title'] = hi(fg.syn_text_title, p.green.ghost),
            ['@text.literal'] = hi(fg.syn_text_literal, p.purple.ghost),
          ['@text.reference'] = hi(fg.syn_text_reference, nil, nil, nil),
@@ -353,15 +537,6 @@ local scheme_group = {
           ['@function.call'] = hi(fg.syn_function1, nil, nil, nil),
        ['@function.builtin'] = hi(fg.syn_function2, nil, nil, nil),
 
-            ['@conditional'] = hi(fg.syn_keyword1, bg.syn_keyword1, nil, nil),
-                 ['@repeat'] = hi(fg.syn_keyword1, bg.syn_keyword1, nil, nil),
-                  ['@label'] = hi(fg.syn_keyword2, bg.syn_keyword2, 'bold', nil),
-               ['@operator'] = hi(fg.syn_keyword2, nil, nil, nil),
-                ['@keyword'] = hi(fg.syn_keyword1, bg.syn_keyword1, nil, nil),
-         ['@keyword.return'] = hi(fg.syn_keyword2, bg.syn_keyword1, nil, nil),
-       ['@keyword.function'] = hi(fg.syn_keyword2, bg.syn_keyword1, nil, nil),
-       ['@keyword.operator'] = hi(fg.syn_keyword1, bg.syn_keyword1, nil, nil), 
-              ['@exception'] = hi(fg.syn_keyword1, bg.syn_keyword1, nil, nil),
 
                ['@variable'] = hi(fg.syn_variable1, nil, nil, nil),
        ['@variable.builtin'] = hi(fg.syn_variable2, nil, nil, nil), 
@@ -379,8 +554,6 @@ local scheme_group = {
               ['@structure'] = hi(fg.syn_type1, nil, nil, nil),
             ['@constructor'] = hi(fg.syn_type1, nil, nil, nil),
 
-              ['@namespace'] = hi(fg.syn_namespace, nil, nil, nil),
-
                 ['@include'] = hi(fg.syn_preproc1, bg.syn_preproc, nil, nil),
                 ['@preproc'] = hi(fg.syn_preproc1, bg.syn_preproc, nil, nil),
                  ['@define'] = hi(fg.syn_preproc1, bg.syn_preproc, nil, nil),
@@ -391,7 +564,8 @@ local scheme_group = {
              ['@annotation'] = hi(fg.syn_metadata, bg.syn_metadata, 'bold', nil),
               ['@attribute'] = hi(fg.syn_metadata, bg.syn_metadata, 'bold', nil),
       
-                   ['@todo'] =  hi(fg.syn_todo, bg.syn_todo, 'bold', nil)
+                   ['@todo'] =  hi(fg.syn_todo, bg.syn_todo, 'bold', nil),
+                   --]]
 }
 
 local function highlight(group, style)
@@ -430,6 +604,8 @@ M.colorscheme = function()
   apply_set(scheme_group)
   apply_set(diagnostic_group)
   apply_set(telescope_group)
+  apply_set(lsp_group)
+  apply_set(treesitter_group)
 --  for group, style in pairs(scheme) do
  --   if (type(style) == 'string') then
   --    link(group, style)
